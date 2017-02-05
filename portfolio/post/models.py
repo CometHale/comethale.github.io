c6d345d6885e312c918016241e7c1afb262ab39b
Haley Sanders <haleyfsanders@gmail.com>
2017-02-04 17:22:14 -0800

    adding new css default.html

--- portfolio/post/models.py
+++
@@ -1,8 +0,0 @@
-from __future__ import unicode_literals
-from django.db import models
-
-class Post(models.Model):
-	text = models.TextField()
-	publish_datetime = models.DateTimeField(auto_now_add=True)
-	edit_datetime = models.DateTimeField(auto_now=True)
-
